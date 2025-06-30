f82c9f042bba1995c6963eda60b8a8d802c8421c
ShaodeNentily <zhellye@googlemail.com>
2025-06-30 17:05:39 +0200

    Update ver_user.php

--- ver_user.php
+++ ver_user.php
@@ -2,6 +2,10 @@
 include 'config.php';
 include 'menu.php';
 
+if (!$loggedin) {
+    header("Location: login.php");
+    exit();
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
